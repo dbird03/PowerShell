d9a7571af31facef54f93e3c15707f7fdf07793c
David Bird <42259193+dbird03@users.noreply.github.com>
2019-02-20 16:02:14 -0500

    Upload UPN and Email Mismatch.ps1

---
+++ UPN and Email Mismatch.ps1
@@ -0,0 +1,19 @@
+﻿<#
+.DESCRIPTION
+This script exports a csv file of AD users in a specified OU whose userPrincipalName attribute does not match their email address.
+
+.NOTES
+    Version: 1.0
+    Author: David Bird
+    Creation Date: July 16, 2018
+
+#>
+
+
+# User-defined variables
+$Date = Get-Date -UFormat "%Y-%m-%d"
+$SearchBase = "OU=Users,DC=contoso,DC=com"
+$ExportCsvLocation = "\\contoso.com\users\username\PowerShell\Exports\AD-Mismatch-$Date.csv"
+
+# Script
+Get-ADUser -Filter * -SearchBase $SearchBase -Properties EmailAddress | Where { $_.UserPrincipalName -ne $_.EmailAddress } | Select Name,SamAccountName,UserPrincipalName,EmailAddress | Export-Csv $ExportCsvLocation -NoTypeInformation
