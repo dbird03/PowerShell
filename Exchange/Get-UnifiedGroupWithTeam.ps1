7181b04159ab0b37b4cd8338d53bb33c4b093403
David Bird <dbird03@gmail.com>
2020-07-07 14:52:49 -0400

    Changed parameter to Identity

--- Exchange/Get-UnifiedGroupWithTeam.ps1
+++ Exchange/Get-UnifiedGroupWithTeam.ps1
@@ -3,19 +3,19 @@ function Get-UnifiedGroupWithTeam {
 .SYNOPSIS
     Gets Office 365 Group
 .DESCRIPTION
-    Takes the Name of an Office 365 Group and checks the ResourceProvisioningOptions property to see if it contains 'Teams' and outputs the
+    Takes the Identity of an Office 365 Group and checks the ResourceProvisioningOptions property to see if it contains 'Teams' and outputs the
     entire Office 365 Group if true.
 .EXAMPLE
-    PS C:\> $AllUnifiedGroups = (Get-UnifiedGroup).Name 
-    PS C:\> Get-UnifiedGroupWithTeam -Name $AllUnifiedGroups    
-    Gets the Name property of all Office 365 Groups then uses Get-UnifiedGroupWithTeam to select only the groups with a Teams team provisioned.
+    PS C:\> $AllUnifiedGroups = (Get-UnifiedGroup).Identity 
+    PS C:\> Get-UnifiedGroupWithTeam -Identity $AllUnifiedGroups    
+    Gets the Identity property of all Office 365 Groups then uses Get-UnifiedGroupWithTeam to select only the groups with a Teams team provisioned.
 .EXAMPLE
-    PS C:\> (Get-UnifiedGroup).Name | Get-UnifiedGroupWithTeam
-    Gets the Name property of all Office 365 Groups and then pipes them to Get-UnifiedGroupWithTeam to select only the groups with a Teams team provisioned.
-.PARAMETER Name
-    The Name property of the Office 365 Group
+    PS C:\> (Get-UnifiedGroup).Identity | Get-UnifiedGroupWithTeam
+    Gets the Identity property of all Office 365 Groups and then pipes them to Get-UnifiedGroupWithTeam to select only the groups with a Teams team provisioned.
+.PARAMETER Identity
+    The Identity property of the Office 365 Group
 .INPUTS
-    Name
+    Identity
 .OUTPUTS
     Deserialized.Microsoft.Exchange.Data.Directory.Management.UnifiedGroupBase
 .NOTES
@@ -26,12 +26,12 @@ function Get-UnifiedGroupWithTeam {
         [Parameter(
             Mandatory = $True,
             ValueFromPipeline = $True)]
-        [string[]]$Name
+        [string[]]$Identity
     )
 
     
     process {
-        foreach ($Item in $Name) {
+        foreach ($Item in $Identity) {
             $UnifiedGroup = Get-UnifiedGroup -Identity $Item
             if ($UnifiedGroup.ResourceProvisioningOptions -contains 'Team') {
                 $UnifiedGroup
