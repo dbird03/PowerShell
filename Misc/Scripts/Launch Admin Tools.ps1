2beb489118d720553e810787df78efca96548ef5
David Bird <42259193+dbird03@users.noreply.github.com>
2021-03-15 12:46:53 -0400

    Updated Launch Admin Tools.ps1

--- Misc/Scripts/Launch Admin Tools.ps1
+++ Misc/Scripts/Launch Admin Tools.ps1
@@ -1,26 +1,29 @@
 <#
+.SYNOPSIS
+    Launches admin tools using elevated credentials.
 .DESCRIPTION
-This script launches your administrator tools using your administrator credentials.
-This is helpful for quickly launching your regularly used tools such as a custom MMC console, System Center Configuration Manager, etc.
-
-.NOTES
-    Version: 1.0
-    Author: David Bird
-    Creation Date: July 29, 2017
-
+    This script prompts for elevated credentials and then launches admin tools using the elevated credentials.
 #>
 
-# Prompt for administrator credentials
-$AdministratorCredentials = Get-Credential -Message "Enter your administrator credentials" -UserName "contoso.com\david.bird"
+# Prompt for admin credentials
+$AdminCreds = Get-Credential -Message "Enter your admin credentials" -UserName "contoso.com\jsmith.admin"
 
-# Open Microsoft Management Console with custom .msc file.
-Start-Process -FilePath "C:\Windows\System32\mmc.exe" -ArgumentList "\\contoso.com\home\dbird\MMC.msc" -WindowStyle Minimized -Credential $AdministratorCredentials
-
-# Open System Center Configuration Manager
-Start-Process "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\Microsoft.ConfigurationManagement.exe" -WindowStyle Minimized -Credential $AdministratorCredentials
+if ($AdminCreds) {
+# Open Microsoft Management Console with custom .msc file
+Start-Process -FilePath "C:\Windows\System32\mmc.exe" -WorkingDirectory "\\contoso.com\home\jsmith" -ArgumentList "Admin MMC.msc" -WindowStyle Minimized -Credential $AdminCreds
 
 # Open Remote Desktop Connection Manager
-Start-Process "C:\Program Files (x86)\Microsoft\Remote Desktop Connection Manager\RDCMan.exe" -WindowStyle Minimized -Credential $AdministratorCredentials
+Start-Process "C:\Program Files (x86)\Microsoft\Remote Desktop Connection Manager\RDCMan.exe" -WindowStyle Minimized -Credential $AdminCreds
 
-# Open PowerShell ISE
-Start-Process "C:\Windows\System32\WindowsPowerShell\v1.0\PowerShell_ISE.exe" -WindowStyle Minimized -Credential $AdministratorCredentials
+# Open Server Manager
+Start-Process "C:\Windows\System32\ServerManager.exe" -WindowStyle Minimized -Credential $AdminCreds
+
+# Open System Center Configuration Manager
+Start-Process "C:\Program Files (x86)\Microsoft Configuration Manager\AdminConsole\bin\Microsoft.ConfigurationManagement.exe" -WindowStyle Minimized -Credential $AdminCreds
+
+# Open Visual Studio Code
+Start-Process -Path "C:\Program Files\Microsoft VS Code\bin\code.cmd" -WindowStyle Minimized -Credential $AdminCreds
+}
+
+# Exit the script
+Exit
