f0509ccea49fbaa2b8eb14cd7cb85ef0f61c5eba
David Bird <42259193+dbird03@users.noreply.github.com>
2021-04-09 11:06:18 -0400

    Updated Help for Import-CustomFunctions.ps1

--- PowerShell/Import-CustomFunctions.ps1
+++ PowerShell/Import-CustomFunctions.ps1
@@ -3,9 +3,19 @@ function Import-CustomFunctions {
 .SYNOPSIS
     Imports custom functions in .ps1 files to the current session.
 .DESCRIPTION
-    This function is used to import custom functions in .ps1 files that are not in proper .psm1 module yet.
+    This function is used to import miscellaneous custom functions in .ps1 files that are not in proper .psm1 module yet.
 
-    IMPORTANT: You need to dot-source this function when using it. See Examples.
+    IMPORTANT: You need to dot-source this function when using it within other functions or from the command line. See Examples for more details.
+
+    Dot-source this function in your PowerShell profile, then create another function to call this function with the Path variable specific to your environment (Ex: If you store the PowerShell function folder on your company's network share.)
+
+    # Profile.ps1
+    . '\\contoso.com\home\Users\dbird\GitHub\PowerShell\PowerShell\Import-CustomFunctions.ps1'
+
+    function icf {
+    . Import-CustomFunctions -Path '\\contoso.com\home\Users\dbird\GitHub\PowerShell' -Verbose
+    }
+    # End Profile.ps1
 .EXAMPLE
     PS C:\> . Import-CustomFunctions -Path 'C:\Scripts\PowerShell'
     Imports the custom functons in the 'C:\Scripts\PowerShell' path.
